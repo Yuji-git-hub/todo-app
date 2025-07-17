48f2f2113ed09d8f8e2a5bb272a9154ca6dff0b4
Yuji <ss.yuji1012@gmail.com>
2025-07-17 20:20:01 +0900

    ログイン画面、ルーティングの追記

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'edit'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
     Route::get('/todos/index', [TodoController::class, 'index'])->name('todos.index');
     Route::get('/todos/create', [TodoController::class, 'create'])->name('todos.create');
     Route::post('/todos/store', [TodoController::class, 'store'])->name('todos.store');
