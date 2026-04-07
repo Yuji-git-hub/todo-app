4145015725e5025d58afd0b94a0e6ce1146c13f4
Yuji <ss.yuji1012@gmail.com>
2026-04-08 07:18:15 +0900

    todo新規作成のblade作成

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -4,18 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo一覧</title>
+    <style>
+        .todo-item::before {content: "・";}
+    </style>
 </head>
 <body>
     <h1>Todo一覧</h1>
 
     <div>
+        <h2>タイトル作成</h2>
+        <form action="/todos" method="post">
+            @csrf
+            <input type="text" name="title" value="{{ old('title') }}">
+            <input type="submit" value="作成">
+        </form>
+    </div>
+
+    <div>
         <table>
             <tr>
-                <th>やることリスト</th>
+                <th><h2>やることリスト</h2></th>
             </tr>
             @foreach($todos as $todo)
             <tr>
-                <td>{{ $todo->title }}</td>
+                <td class="todo-item">{{ $todo->title }}</td>
             </tr>
             @endforeach
         </table>
