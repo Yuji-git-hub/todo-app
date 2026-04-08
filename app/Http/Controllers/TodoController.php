7c5f86072e96aa9ba40db6d998790b138c8ee663
Yuji <ss.yuji1012@gmail.com>
2026-04-08 09:20:34 +0900

    edit,update,deleteメソッドの追加

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -18,6 +18,28 @@ class TodoController extends Controller
     {
         Todo::create($request->validated());
 
-        return redirect()->route('todos.index');
+        return redirect()->route('todos.index')
+                         ->with('success', 'リストが作成されました。');
+    }
+
+    public function edit(Todo $todo)
+    {
+        return view('todos.edit', ['todo' => $todo]);
+    }
+
+    public function update(TodoRequest $request, Todo $todo)
+    {
+        $todo->update($request->validated());
+
+        return redirect()->route('todos.index')
+                         ->with('success', '更新されました。');
+    }
+
+    public function delete(Todo $todo)
+    {
+        $todo->delete();
+
+        return redirect()->route('todos.index')
+                         ->with('success', '削除されました。');
     }
 }
