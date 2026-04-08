a666dff8d773acab5412bee28f2c733ac23fa01f
Yuji <ss.yuji1012@gmail.com>
2026-04-08 09:24:59 +0900

    ルーティングの追記

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 
 Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
 Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+Route::get('/todos/edit/{todo}', [TodoController::class, 'edit'])->name('todos.edit');
+Route::put('todos/edit/{todo}', [TodoController::class, 'update'])->name('todos.update');
+Route::delete('todos/delete', [TodoController::class, 'delete'])->name('todos.delete');
